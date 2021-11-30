58c368d4051eb9b58e4e91dd96f2a9fb5b0cf99d
RTC <mikhail.bei@web.de>
2021-11-30 20:49:27 +0100

    websocket_1

--- src/app/list-expressions/list-expressions.component.ts
+++ src/app/list-expressions/list-expressions.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ExpressionService } from '../_services/expression.service';
 import { ExpressionDTO } from '../_models/expressionDTO';
+import { WebsocketService } from '../_services/websocket.service';
 
 @Component({
   selector: 'app-list-expressions',
@@ -9,17 +10,16 @@ import { ExpressionDTO } from '../_models/expressionDTO';
 })
 export class ListExpressionsComponent implements OnInit {
   
-  loading = false;
-  expressions!: ExpressionDTO[];
-
-  constructor(private expressionService: ExpressionService) { }
+  constructor(public webSocket: WebsocketService) {
+    
+  }
 
   ngOnInit() {
-    this.loading = true;
-    this.expressionService.getAll().subscribe(expressions => {
+
+    /* this.expressionService.getAll().subscribe(expressions => {
         this.loading = false;
         this.expressions = expressions;
-    });
+    }); */
   }
 
 }
